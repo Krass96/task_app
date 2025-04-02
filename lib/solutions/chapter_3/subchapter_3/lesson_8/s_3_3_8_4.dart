f9d19f2b6902087298f1b02dfed608727209460a
Krass96 <gyncoo@abv.bg>
2025-04-02 14:46:56 +0200

    s3384

--- lib/solutions/chapter_3/subchapter_3/lesson_8/s_3_3_8_4.dart
+++ lib/solutions/chapter_3/subchapter_3/lesson_8/s_3_3_8_4.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 
 int countVowels(String input) {
   // Lösung hier einfügen
-  throw UnimplementedError();
+  int count = 0;
+  final vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+
+  for (int i = 0; i < input.length; i++) {
+    if (vowels.contains(input[i])) {
+      count++;
+    }
+  }
+
+  return count;
 }
 
 class S3384 extends StatefulWidget {
