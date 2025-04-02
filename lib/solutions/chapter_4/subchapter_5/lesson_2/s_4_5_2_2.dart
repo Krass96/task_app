e87838669a4104b7f60110b01966e800b2f3b1e8
Krass96 <gyncoo@abv.bg>
2025-04-02 12:15:16 +0200

    s4522

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -4,6 +4,24 @@ class S4522 extends StatelessWidget {
   const S4522({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyStatelessWidget();
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('Hello Krasimir Varnaliev'),
+        SizedBox(height: 10),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text('Klick mich'),
+        ),
+      ],
+    );
   }
 }
