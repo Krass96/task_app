0f7ec64dd114c8e4eafb9d92ab785a90f990b387
Krass96 <gyncoo@abv.bg>
2025-04-03 14:25:00 +0200

    s4535

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_5.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_5.dart
@@ -1,9 +1,37 @@
 import 'package:flutter/material.dart';
+import 'Klassen/container_example.dart';
 
 class S4535 extends StatelessWidget {
   const S4535({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ColorfulRectangle();
+  }
+}
+
+class ColorfulRectangle extends StatelessWidget {
+  const ColorfulRectangle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ContainerExample(color: Colors.red),
+            ContainerExample(color: Colors.green),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ContainerExample(color: Colors.blue),
+            ContainerExample(color: Colors.yellow),
+          ],
+        ),
+      ],
+    );
   }
 }
