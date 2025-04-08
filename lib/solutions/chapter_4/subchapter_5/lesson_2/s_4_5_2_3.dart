693325df55a7de48d5c31130eb7f70243117191d
Krass96 <gyncoo@abv.bg>
2025-04-08 13:00:28 +0200

    s4523

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
@@ -4,6 +4,38 @@ class S4523 extends StatelessWidget {
   const S4523({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyNameWidget();
+  }
+}
+
+class MyNameWidget extends StatefulWidget {
+  const MyNameWidget({super.key});
+
+  @override
+  State<MyNameWidget> createState() => _MyNameWidgetState();
+}
+
+class _MyNameWidgetState extends State<MyNameWidget> {
+  String name = '';
+
+  void schowName() {
+    setState(() {
+      name = 'Krasimir Varnaliev';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(name),
+        SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: () => schowName(),
+          child: Text('Name anzeigen'),
+        ),
+      ],
+    );
   }
 }
