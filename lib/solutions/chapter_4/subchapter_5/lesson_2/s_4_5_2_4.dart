c7b596c22926351d80799ee55479ba1ee8ab2286
Krass96 <gyncoo@abv.bg>
2025-04-08 13:22:04 +0200

    s4524

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
@@ -3,7 +3,33 @@ import 'package:flutter/material.dart';
 class S4524 extends StatelessWidget {
   const S4524({super.key});
   @override
-  Widget build(BuildContext context) {
-    throw UnimplementedError();
-  }
+  Widget build(BuildContext context) => ShowHideNameWidget();
+}
+
+class ShowHideNameWidget extends StatefulWidget {
+  const ShowHideNameWidget({super.key});
+
+  @override
+  State<ShowHideNameWidget> createState() => _ShowHideNameWidgetState();
+}
+
+class _ShowHideNameWidgetState extends State<ShowHideNameWidget> {
+  String name = '';
+  String buttonText = 'Name anzeigen';
+
+  void toggleName() => setState(() {
+        name = name.isEmpty ? 'Krasimir Varnaliev' : '';
+        buttonText = name.isNotEmpty ? 'Name verstecken' : 'Name anzeigen';
+      });
+
+  @override
+  Widget build(BuildContext context) => Column(
+        children: [
+          Text(name),
+          SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(onPressed: toggleName, child: Text(buttonText)),
+        ],
+      );
 }
